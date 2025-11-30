a9baf363e1c05506f6b1634a47ec4e422e8f7824
togura <>
2025-11-30 14:36:47 +0000

    fix: add error logging to fetchData function

--- terraform-UI/src/Bord/request.tsx
+++ terraform-UI/src/Bord/request.tsx
@@ -6,7 +6,9 @@ async function fetchData(prefix: string = "", data: any = null) {
   try {
     const response = await axios.get(`${BASE_URL}${prefix}`, { params: data });
     return response.data;
-  } catch (err) {}
+  } catch (err) {
+    console.log("通信失敗:", err);
+  }
 }
 
 export default fetchData;
